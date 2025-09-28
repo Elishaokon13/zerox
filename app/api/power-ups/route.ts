56d2061ebe7dd909bbb652d0b2c1c99919b79dc7
Elishaokon13 <eliscodavid@gmail.com>
2025-09-28 23:13:20 +0100

    Modified app/api/power-ups/route.ts

--- app/api/power-ups/route.ts
+++ app/api/power-ups/route.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextRequest, NextResponse } from 'next/server';
-import { supabase } from '@/lib/supabase';
+import { supabase, supabaseAdmin } from '@/lib/supabase';
 
 export async function POST(req: NextRequest) {
   try {
