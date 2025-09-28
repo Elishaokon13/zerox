ef097fc664ea462ab2dc707337fbdc38245cfbbe
Elishaokon13 <eliscodavid@gmail.com>
2025-09-29 00:17:29 +0100

    Modified next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,7 @@ const nextConfig = {
       { protocol: 'https', hostname: 'api.dicebear.com' },
       { protocol: 'https', hostname: 'tba-mobile.mypinata.cloud' },
       { protocol: 'https', hostname: 'farcaster.xyz' },
+      { protocol: 'https', hostname: 'ipfs.decentralized-content.com' },
     ],
   },
 };
